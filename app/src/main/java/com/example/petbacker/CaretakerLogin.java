e31e2fbe06173b54ef098585ca6da5f1b21d5089
Ashen Sam <dilshiperera2580@gmail.com>
2024-05-23 20:37:22 +0530

    initial commit

---
+++ app/src/main/java/com/example/petbacker/CaretakerLogin.java
@@ -0,0 +1,14 @@
+package com.example.petbacker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CaretakerLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caretaker_login);
+    }
+}
---
+++ app/src/main/java/com/example/petbacker/petownerMenu.java
@@ -0,0 +1,14 @@
+package com.example.petbacker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class petownerMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_petowner_menu);
+    }
+}
